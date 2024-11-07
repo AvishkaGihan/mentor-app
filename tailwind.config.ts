7233fb5b513517e15b3e007e5c6d0bb637527479
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-11-07 23:12:37 +0530

    enhance Tailwind CSS configuration with new gradient backgrounds and pastel colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-sunburst": "radial-gradient(circle at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-bluemist": "linear-gradient(to bottom, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "aqua-pastel": "#D0F4F5",
+        "aqua-pastel-light": "#E6FBFC",
+        "lavender-pastel": "#E4DBFF",
+        "lavender-pastel-light": "#F2F0FF",
+        "sunshine-pastel": "#FFF6D9",
+        "sunshine-pastel-light": "#FFFDF2",
       },
     },
   },
