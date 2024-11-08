982a37e5be64ce9c275ae10c2ad3df4ba6ecb9d0
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-11-08 20:09:13 +0530

    add CountChart component and integrate it into AdminPage layout

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -11,6 +12,14 @@ const AdminPage = () => {
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
+        </div>
+
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
         </div>
       </div>
 
