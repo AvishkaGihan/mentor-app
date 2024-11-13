5a46d543277805dc58c60da6f5b2370e6db1cce2
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-11-13 11:36:09 +0530

    add InputField component; update FormModal for create and delete actions; enhance package dependencies

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -86,9 +86,6 @@ const TeacherListPage = () => {
             </button>
           </Link>
           {role === "admin" && (
-            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-purple">
-            //   <Image src="/delete.png" alt="" width={16} height={16} />
-            // </button>
             <FormModal table="teacher" type="delete" id={item.id} />
           )}
         </div>
