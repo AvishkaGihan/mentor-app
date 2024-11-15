0f25d458108063a0020764d2a147156ed10938b3
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-11-15 16:06:05 +0530

    refactor StudentListPage to use Prisma for data fetching; update renderRow to handle StudentList type and include class details; implement pagination and search functionality

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -145,7 +145,10 @@ const SingleTeacherPage = () => {
             <Link className="p-3 rounded-md bg-skyLight" href="/">
               Teacher&apos;s Classes
             </Link>
-            <Link className="p-3 rounded-md bg-purpleLight" href="/">
+            <Link
+              className="p-3 rounded-md bg-purpleLight"
+              href={`/list/students?teacherId=${"teacher2"}`}
+            >
               Teacher&apos;s Students
             </Link>
             <Link className="p-3 rounded-md bg-yellowLight" href="/">
