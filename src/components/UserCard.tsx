885988e8b30a3d6165af6099164f0ac40eff9c28
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-11-28 10:54:36 +0530

    refactor UserCard component; update user type handling to use Prisma for data fetching; adjust AdminPage to reflect correct user roles

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,22 @@
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-const UserCard = ({ type }: { type: string }) => {
+type PrismaModel = {
+  count: () => Promise<number>;
+};
+
+const modelMap: Record<UserCardType, PrismaModel> = {
+  admin: prisma.admin as PrismaModel,
+  teacher: prisma.teacher as PrismaModel,
+  student: prisma.student as PrismaModel,
+  parent: prisma.parent as PrismaModel,
+};
+
+type UserCardType = "admin" | "teacher" | "student" | "parent";
+
+const UserCard = async ({ type }: { type: UserCardType }) => {
+  const data = await modelMap[type].count();
+
   return (
     <div className="rounded-2xl odd:bg-purple even:bg-yellow p-4 flex-1 min-w-[130px]">
       <div className="flex justify-between items-center">
@@ -9,7 +25,7 @@ const UserCard = ({ type }: { type: string }) => {
         </span>
         <Image src="/more.png" alt="" width={20} height={20} />
       </div>
-      <h1 className="text-2xl font-semibold my-4">1,234</h1>
+      <h1 className="text-2xl font-semibold my-4">{data}</h1>
       <h2 className="capitalize text-sm font-medium text-gray-500">{type}s</h2>
     </div>
   );
